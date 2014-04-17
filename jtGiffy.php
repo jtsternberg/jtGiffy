a3e4c11f1097893baea5bd5bddb756580b863a99
Justin Sternberg <justin@dsgnwrks.pro>
2014-04-17 09:06:23 -0400

    add it

---
+++ jtGiffy.php
@@ -0,0 +1,73 @@
+<?php
+/*
+Plugin Name: Giffy JSON endpoint
+Plugin URI: http://jtsternberg.com/?gifs
+Description: Give JSON access to your gifs. C'mon, share the love!
+Author URI: http://jtsternberg.com
+Author: Jtsternberg
+Donate link: http://dsgnwrks.pro/give/
+Version: 0.1.0
+*/
+
+class jtGiffy {
+
+	public function __construct() {
+
+		// Save your path via ?jtgiffy_path=/Users/jt/Sites/wpengine/
+		if ( isset( $_GET['jtgiffy_url'] ) ) {
+			update_option( 'jtgiffy_url', esc_url( $_GET['jtgiffy_url'] ) );
+		}
+		$this->gif_url = get_option( 'jtgiffy_url', site_url( '/gifs' ) );
+		$this->gif_path = trailingslashit( str_ireplace( site_url(), untrailingslashit( ABSPATH ), $this->gif_url ) ) .'*.gif';
+
+	}
+
+	public function hooks() {
+		add_action( 'template_redirect', array( $this, 'get_gifs' ), 9999 );
+	}
+
+	public function get_gifs() {
+		$gifs = glob( $this->gif_path );
+		if ( empty( $gifs ) )
+			return false;
+
+		$gifs = $this->gif_urls( $gifs );
+
+		// Halt here for json
+		if ( isset( $_GET['json'] ) ) {
+			wp_send_json_success( $gifs );
+		}
+
+		return $gifs;
+	}
+
+	public function gif_urls( $gif_paths ) {
+		$gifs = array();
+		foreach ( $gif_paths as $gif ) {
+			$filename = explode( '/', $gif );
+			$filename = array_pop( $filename );
+
+			// Filter out if a term was searched & json
+			if ( isset( $_GET['json'] ) && $_GET['gifs'] && false === stripos( $filename, $_GET['gifs'] ) ) {
+				continue;
+			}
+
+			$nice_name = explode( '.', $filename );
+			$nice_name = array_shift( $nice_name );
+			$src = esc_url( str_ireplace( ABSPATH, site_url( '/' ), $gif ) );
+
+			$gifs[ $filename ] = array(
+				'name' => str_ireplace( '-', ' ', $nice_name ),
+				'src'  => $src,
+			);
+		}
+		return $gifs;
+	}
+
+}
+
+$jtGiffy = new jtGiffy();
+
+if ( isset( $_GET['gifs'], $_GET['json'] ) ) {
+	$jtGiffy->hooks();
+}
