c7253397e8dad39cb77f518a61bea4c231e6e138
Greg Rickaby <gregrickaby@gmail.com>
2014-04-25 12:37:20 -0400

    Add usage instructions to the description

--- jtGiffy.php
+++ jtGiffy.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Giffy JSON endpoint
 Plugin URI: http://jtsternberg.com/?gifs&json
-Description: Give JSON access to your gifs. C'mon, share the love!
+Description: Give JSON access to your gifs. C'mon, share the love! To set the location of your gifs, go to your site and add ?jtgiffy_url=http://your-site.com/gifs to the url. This will save the option with the location of your gif folder.
 Author URI: http://jtsternberg.com
 Author: Jtsternberg
 Donate link: http://dsgnwrks.pro/give/
